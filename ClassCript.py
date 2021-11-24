d9bc9c16c24db8cb60578960da34f2f47dd09a0a
Armlliday <dhalliday@unal.edu.co>
2021-11-24 17:33:10 -0500

    Added the crypto classes for the shift and vigenere ciphers

--- ClassCript.py
+++ ClassCript.py
@@ -1,4 +1,6 @@
 from random import sample
+import numpy as np
+from collections import Counter
 
 
 class CripSustitucion():
@@ -96,3 +98,108 @@ class CripPermutacion():
             list1 = list(dat[i * self.m:(i + 1) * self.m])
             listfinal.append(list1)
         return listfinal
+
+
+class CripDesplazamiento():
+
+    def __init__(self, data, m):
+        self.data = data
+        self.m = m
+        self.flush = []
+
+    def encriptar(self):
+        if not self.data.isalpha():
+            return "Unacceptable input"
+        word_ascii = np.array([ord(c) for c in self.data.replace(" ", "").lower()])
+        word_encryption = (((word_ascii - 97) + self.m) % 26) + 97
+        encryption = [chr(c) for c in word_encryption]
+        return ''.join(encryption).upper()
+
+    def desencriptar(self):
+        word_ascii = np.array([ord(c) for c in self.data.lower()])
+        word_decryption = (((word_ascii - 97) - self.m) % 26) + 97
+        decryption = [chr(c) for c in word_decryption]
+        return ''.join(decryption)
+
+    def criptanalisis(self):
+        possible_words = []
+        for i in range(26):
+            self.m = i
+            possible_words.append(self.desencriptar())
+        return possible_words
+
+
+class CripVigenere():
+
+    def __init__(self, data,key):
+        self.data = data.replace(" ", "")
+        self.key = key
+
+    def encriptar(self):
+        if not self.data.isalpha():
+            return "Unacceptable input"
+        word_ascii = np.array([ord(c) for c in self.data.lower()])
+        for i in range(len(self.key)):
+            pos = i
+            while pos < len(self.data):
+                word_ascii[pos] = (((word_ascii[pos] - 97) + (ord(self.key.lower()[i]) - 97)) % 26) + 97
+                pos += len(self.key)
+        encryption = [chr(c) for c in word_ascii]
+        return ''.join(encryption).upper()
+
+    def desencriptar(self):
+        word_ascii = np.array([ord(c) for c in self.data.lower()])
+        for i in range(len(self.key)):
+            pos = i
+            while pos < len(self.data):
+                word_ascii[pos] = (((word_ascii[pos] - 97) - (ord(self.key.lower()[i]) - 97)) % 26) + 97
+                pos += len(self.key)
+        encryption = [chr(c) for c in word_ascii]
+        return ''.join(encryption)
+
+    def ic(self, x):
+        freq = Counter(x)
+        sum = 0
+        for i in freq:
+            sum += freq[i] * (freq[i] - 1)
+        return sum / (len(x) * (len(x) - 1))
+
+    def ic_average(self, x, i):
+        wordsAv = []
+        for j in range(i):
+            wordsAv.append(self.ic(x[j::i]))
+        return np.mean(wordsAv)
+
+    def mg(self, word, n):
+        indices = [0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015,
+                   0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749,
+                   0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758,
+                   0.00978, 0.02360, 0.00150, 0.01974, 0.00074]
+        freq = {}
+        for c in 'abcdefghijklmnopqrstuvwxyz':
+            freq[c] = 0
+        temp = Counter(''.join(word.lower()))
+        for a in temp:
+            freq[a] += temp[a]
+        m = []
+        for g in range(26):
+            sum = 0
+            for i in range(26):
+                sum += indices[i] * freq[chr(((i + g) % 26) + 97)]
+            m.append((g, abs(0.0667 - (sum / n))))
+        return chr((sorted(m, key=lambda x: x[1])[0][0]) + 97)
+
+    def criptanalisis(self):
+        average = []
+        for i in range(20):
+            average.append((i + 1, abs(0.0667 - self.ic_average(self.data, i + 1))))
+        possibles_ordered = sorted(average, key=lambda x: x[1])
+        return [i[0] for i in possibles_ordered]
+
+    def criptanalisis_key(self,m):
+        possible_key = ''
+        for i in range(m):
+            y = self.data.lower()[i::m]
+            possible_key += self.mg(y, len(self.data) / m)
+        self.key = possible_key
+        return possible_key
--- main.py
+++ main.py
@@ -16,9 +16,23 @@ def print_hi(name):
 if __name__ == '__main__':
     abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     s = "holascomosestasbeb"
-    example = CripPermutacion(s,5)
+    example = CripPermutacion(s, 5)
     print(example.encriptar())
     print(example.desencriptar())
+    ex= CripDesplazamiento(s,5)
+    print(ex.encriptar())
+    ex2=CripDesplazamiento(ex.encriptar(), 5)
+    print(ex2.desencriptar())
+    print(ex2.criptanalisis())
+    ex3=CripVigenere(s, 'cipher')
+    print(ex3.encriptar())
+    ex4=CripVigenere(ex3.encriptar(),'cipher')
+    print(ex4.desencriptar())
+    stri = 'CHREEVOAHMAERATBIAXXWTNXBEEOPHBSBQMQEQERBWRVXUOAKXAOSXXWEAHBWGJMMQMNKGRFVGXWTRZXWIAKLXFPSKAUTEMNDCMGTSXMXBTUIADNGMGPSRELXNJELXVRVPRTULHDNQWTWDTYGBPHXTFALJHASVBFXNGLLCHRZBWELEKMSJIKNBHWRJGNMGJSGLXFEYPHAGNRBIEQJTAMRVLCRREMNDGLXRRIMGNSNRWCHRQHAEYEVTAQEBBIPEEWEVKAKOEWADREMXMTBHHCHRTKDNVRZCHRCLQOHPWQAIIWXNRMGWOIIFKEE'
+    crypta = CripVigenere(stri, '')
+    print(crypta.criptanalisis())
+    print(crypta.criptanalisis_key(crypta.criptanalisis()[0]))
+    print(crypta.desencriptar())
 
 
 

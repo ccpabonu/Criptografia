6e198ac7f8e50a67835e0ba4c19aac7544b49053
Armlliday <dhalliday@unal.edu.co>
2021-11-25 20:05:58 -0500

    Fixed the UI linkage

--- GIU/main_Windows.py
+++ GIU/main_Windows.py
@@ -12,14 +12,19 @@ class main_Windows(QMainWindow):
         uic.loadUi("mainWin.ui", self)
         self.bCripClasica.clicked.connect(self.abrir)
 
+    def abrir(self):
+        widget.setCurrentIndex(widget.currentIndex()+1)
+        cripDesp = main_cripDesp()
+        widget.addWidget(cripDesp)
 
-    def abrir (self):
-        self.ventana = QtWidgets.QMainWindow()
-        self.ui=Ui_cripConve()
-        self.ui.setupUi(self.ventana)
-        self.ventana.show()
+#
+#    def abrir (self):
+#        self.ventana = QtWidgets.QMainWindow()
+#        self.ui=Ui_cripConve()
+#        self.ui.setupUi(self.ventana)
+#        self.ventana.show()
 #        self.ventana.bDesplazamiento.clicked.connect(self.abrirDesp)
-        self.close()
+#        self.close()
 
 
 #    def abrirDesp(self):
@@ -28,10 +33,47 @@ class main_Windows(QMainWindow):
 #        self.ui2.setupUi(self.ventana_d)
 #        self.ventana_d.show()
 #        self.ventana.close()
+class main_cripConve(QMainWindow):
 
+    def __init__(self):
+        super(main_cripConve, self).__init__()
+        uic.loadUi("cripConve.ui", self)
+        self.bDesplazamiento.clicked.connect(self.abrirDesp)
+
+
+    def abrirDesp(self):
+        widget.setCurrentIndex(widget.currentIndex()+1)
+        cripDespC = main_cripDespC()
+        widget.addWidget(cripDespC)
+
+
+class main_cripDesp(QMainWindow):
+
+    def __init__(self):
+        super(main_cripDesp, self).__init__()
+        uic.loadUi("desp.ui", self)
+        self.cripAn.clicked.connect(self.abrirDespC)
+
+    def abrirDespC(self):
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+        #cripDespC = main_cripDespC()
+        #widget.addWidget(cripDespC)
+
+
+class main_cripDespC(QMainWindow):
+
+    def __init__(self):
+        super(main_cripDespC, self).__init__()
+        uic.loadUi("despCriAn.ui", self)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    GUI = main_Windows()
-    GUI.show()
+    widget = QtWidgets.QStackedWidget()
+    mainW = main_Windows()
+    cripC = main_cripConve()
+    widget.addWidget(mainW)
+    widget.addWidget(cripC)
+    widget.setFixedHeight(613)
+    widget.setFixedWidth(1000)
+    widget.show()
     sys.exit(app.exec())

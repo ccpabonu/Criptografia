7fd437c47848fa0886ceb54ac63034c5e0d162dd
Armlliday <dhalliday@unal.edu.co>
2021-11-26 12:42:55 -0500

    Fixed the UI connections and Christian's window adaptability

--- GIU/main_Windows.py
+++ GIU/main_Windows.py
@@ -1,11 +1,14 @@
 import sys
-from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from random import sample
 
-from ClassCript import CripDesplazamiento
-from cripConve import *
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
+
+from ClassCript import CripDesplazamiento, CripSustitucion
+#from cripConve import *
 from GIU.desp import *
 from GIU.despCriAn import *
+
 
 class main_Windows(QMainWindow):
 
@@ -26,12 +29,95 @@ class main_cripConve(QMainWindow):
         super(main_cripConve, self).__init__()
         uic.loadUi("cripConve.ui", self)
         self.bDesplazamiento.clicked.connect(self.abrirDesp)
+        self.bSustitucion.clicked.connect(self.abrirCripSust)
 
 
     def abrirDesp(self):
         cripDesp = main_cripDesp()
         widget.addWidget(cripDesp)
         widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def abrirCripSust(self):
+        cripSus = main_cripConveSustV()
+        widget.addWidget(cripSus)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+class main_cripConveSustV(QMainWindow):
+
+    def __init__(self, parent = None):
+        self.flush = []
+        super(main_cripConveSustV, self).__init__()
+        uic.loadUi("cripConveSust.ui", self)
+        self.bGenerar.clicked.connect(self.generarV)
+        self.bEncriptar.clicked.connect(self.encriptarV)
+        self.bDesencriptar.clicked.connect(self.desEncriptarV)
+        self.bAtras.clicked.connect(self.salirV)
+
+
+    def generarV(self):
+        data = self.lineEdit.text()
+        data2 = list(data)
+        self.flush= sample(data2,26)
+        self.lineEdit.setText("".join(self.flush))
+        self.lineEdit.setDisabled(True)
+
+    def encriptarV(self):
+        data = self.textEdit.toPlainText().lower()
+        data = data.replace('\n',"")
+        self.textEdit.setPlainText(data)
+
+        if len(data)!=0 :
+            flush = self.lineEdit.text()
+            b = list(flush)
+            self.flush = b
+            crip1 = CripSustitucion(data,b)
+            dataEncrip = crip1.encriptar()
+            self.textEdit_2.setPlainText(dataEncrip)
+        else :
+            vent2 = main_errorDialogV(self)
+            vent2.tipoError("No hay texto que cifrar")
+            vent2.show()
+
+    def desEncriptarV(self):
+        self.lineEdit.setDisabled(False)
+        data = self.textEdit_2.toPlainText().upper()
+        data = data.replace('\n', "")
+        self.textEdit_2.setPlainText(data)
+
+        if len(data)!=0 :
+            flush = self.lineEdit.text()
+            b = list(flush)
+            self.flush = b
+            crip1 = CripSustitucion(data,b)
+            dataEncrip = crip1.desencriptar()
+            self.textEdit.setPlainText(dataEncrip)
+
+        else :
+            vent2 = main_errorDialogV(self)
+            vent2.tipoError("No hay texto que cifrar")
+            vent2.show()
+
+
+
+
+    def salirV(self):
+        vent2 = main_cripConve(self)
+        vent2.show()
+        self.hide()
+
+class main_errorDialogV(QDialog):
+    def __init__(self):
+        super(main_errorDialogV, self).__init__()
+        uic.loadUi("errorDialog.ui", self)
+        self.bOk.clicked.connect(self.close)
+
+        #forma de llamar este dialogo de error en sus ventanas
+        '''vent2 = errorDialogV(self)
+        vent2.tipoError("505")
+        vent2.show()'''
+
+    def tipoError(self, error):
+        self.label.setText(error)
 
 
 class main_cripDesp(QMainWindow):
@@ -84,6 +170,7 @@ class main_cripDespC(QMainWindow):
         self.cript.m = 25
         words += self.cript.desencriptar()
         self.caOut.setText(words)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

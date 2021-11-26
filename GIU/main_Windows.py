0bd45e9aa4d112b2e65cc0c4e6eb31840326de28
Armlliday <dhalliday@unal.edu.co>
2021-11-25 19:33:18 -0500

    Working on the UI

--- GIU/main_Windows.py
+++ GIU/main_Windows.py
@@ -2,6 +2,9 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from cripConve import *
+from GIU.desp import *
+from GIU.despCriAn import *
+
 class main_Windows(QMainWindow):
 
     def __init__(self):
@@ -15,17 +18,16 @@ class main_Windows(QMainWindow):
         self.ui=Ui_cripConve()
         self.ui.setupUi(self.ventana)
         self.ventana.show()
-        self.ventana.bDesplazamiento.clicked.connect(self.ventana.abrir_desp())
+#        self.ventana.bDesplazamiento.clicked.connect(self.abrirDesp)
         self.close()
 
 
- #   def abrirDesp(self):
- #       self.ventana_d = QtWidgets.QMainWindow()
- #       self.ui=Ui_desp()
- #       self.ui.setupUi(self.ventana_d)
- #       self.ventana_d.show()
- #       self.close()
-
+#    def abrirDesp(self):
+#        self.ventana_d = QtWidgets.QMainWindow()
+#        self.ui2=Ui_cripDesp()
+#        self.ui2.setupUi(self.ventana_d)
+#        self.ventana_d.show()
+#        self.ventana.close()
 
 
 if __name__ == '__main__':

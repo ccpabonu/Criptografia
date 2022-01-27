4cc00bd74374e03c8c89b78913c78a426f88144d
Christian Camilo Pabón Useche <Christian.pabon@pevaar.com>
2022-01-27 08:42:02 -0500

    SE CREA CLASE RSA

--- main.py
+++ main.py
@@ -5,37 +5,11 @@ import imageio as iio2
 import numpy as np
 import matplotlib.pyplot as plt
 #from skimage import img_as_ubyte
-
+from ClassRSA import ClassRSA
 from ProcIMG import ProcIMG
 from AESIMG import ProcIMG, HillIMG
 
 if __name__ == '__main__':
 
-    #p=[[[0,255,0],[255,0,255],[230,1,2],[0, 255, 0]],
-    #  [[0,255,0],[255,0,255],[1,1,1],[0, 255, 0]]]
-    #m,n=4,2
-    #p = iio.imread('test3.png')
-    #q = iio.imread('result1.png')
-    #n, m, b = p.shape
-    #p=list(p)
-    #crip = ProcIMG(p,m,n)
-    #crip.codificar3Des64CTR()
-    #q= crip.darResultado()
-    #print(q)
-    #iio.imsave('result3.png',q)
-    #print("a decodificar")
-    #crip.decodificar3Des64CTR()
-    #r= crip.darResultado()
-    #print(r)
-    #iio2.imsave('test3-1.png',r)
-    # iio2.imsave('test3-1.png',r)
-    #p = iio.imread('test3.png')
-    q = iio.imread('resultH.png')
-    crip = HillIMG(q, 'lidh')
-    iio.imsave('resultHd.png', crip.d_hill())
-    #a = [213, 5, 29,123,3,2]
-    #crip = CripHill2('', 'lidh')
-    #crip.setObject()
-    #print(crip.boo)
-    #crip.data = a
-    #print(crip.encriptar())
+    crip = ClassRSA()
+    print(crip.q*crip.p)

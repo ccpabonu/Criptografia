d73a32dda44888ec11fdfe3634a8e77fba3780b1
Christian Camilo Pabón Useche <Christian.pabon@pevaar.com>
2022-02-03 03:48:32 -0500

    LOGICA GAMAL

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-
+from ClassGamal import ClassGamal
 from ClassRSA import ClassRSA
 import math as m
 
 if __name__ == '__main__':
 
-    crip = ClassRSA()
-    #crip.generarKey()
-    crip.kPublica = (307001,208091)
-    crip.kPrivada = (307001, 188375)
-    print(crip.p, crip.q,(crip.p-1)*(crip.q-1), crip.n, crip.e)
-    c=crip.encriptar("12345678")
-    print(c)
-    print(type(c[0]))
-    c2 = (''.join(map(lambda x: str(x), c)))
-    print(c2)
-    print(crip.desencriptar(c))
+    crip = ClassGamal()
+    #crip.puntosElip()
+    #s = crip.proEscalar([24,5],13)
+    #print(crip.textToInt("hola"))
+    #print(crip.cifrar("H"))
+    print(crip.descifrar([[[4, 28], [24, 26]]]))
+
+
 
